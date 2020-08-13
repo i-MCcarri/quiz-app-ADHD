ea2aea1055ab4359c06fdaa8924e69a15cd2a722
Adyceum A. Magna Ccarri <61805757+i-MCcarri@users.noreply.github.com>
2020-08-13 07:03:29 -0700

    first commit

---
+++ store.js
@@ -0,0 +1,74 @@
+// Create your initial store
+const popQuiz = [
+    {
+        question: 'What does the acronym A.D.H.D. stand for?',
+        answer: ['Addictive Diabulimia Hyperphagial Diabetes', 'Adult Dialectic Hysteriacal Doubiety', 'Attention Defecit Hyperactivity Disorder', 'Autistic Developmental Hyperexcitable Displacement'],
+        correctAnswer: 'Attention Defecit Hyperactivity Disorder',
+        view: 'multiple-choice',
+        pointValue: 10,
+    },
+    {
+        question: 'Medication is only one of the many methods, of treating the symptoms of A.D.H.D.. What other holistic methods, when paired with medication, have shown great success in treating A.D.H.D.?',
+        answer: ['Routine Exercise', 'Support', 'Nutrition', 'Personalized Natural Supplements']
+        correctAnswer: 'Routine Exercise', 'Support', 'Nutrition', 'Personalized Natural Supplements'
+        view: 'multiple-answer',
+        pointValue: 10,
+    },
+    {
+        qquestion: '30 - 65% of children diagnosed with A.D.H.D. will have disabling symptomsinto adulthood?',
+        answer: ['true', 'false',],
+        correctAnswer: 'true',
+        view: 'boolean',
+        pointValue: 10,
+    },
+    {
+        question: 'Hundreds of studies conclude that the ________ , _______  and _______ shut down, when the patients tried to concentrate, focus or follow-through with a task',
+        answer: ['Grey Matter', 'Cerebellum', 'Hippocampus', 'Prefrontal Cortex'],
+        correctAnswer: 'Cerebellum', 'Prefrontal Cortex',
+        view: 'multiple-answer',
+        pointValue: 10,
+    },
+    {
+        question: 'A.D.H.D. has been found in every country it has been studied. It is not caused by bad parenting or teaching.',
+        answer: ['true', 'false',],
+        correctAnswer: 'true',
+        view: 'boolean',
+        pointValue: 10,
+    },
+    {
+        question: 'The national average of people who drop out of high school is _____ ! With that in mind, _____ of people with A.D.H.D. never finish High School.',
+        answer: ['9%, 32%', '10%, 3%', '8%, 70%', '7%, 45%'],
+        correctAnswer: '9%, 32%',
+        view: 'multiple-choice',
+        pointValue: 10,
+    },
+    {
+        question: 'A.D.D. is now called "inattentive" A.D.H.D.',
+        answer: ['True', 'False',],
+        correctAnswer: 'true',
+        view: 'boolean',
+        pointValue: 10,
+    },
+    {
+        question: 'A.D.H.D. is more common in ______ .',
+        answer: ['Women', 'Men',],
+        correctAnswer: 'Women',
+        view: 'multiple-choice',
+        pointValue: 10,
+    },
+    {
+        question: 'Inattentive A.D.H.D. is associated with ______  along with a tremendous sense of under acchievement and lonliness.',
+        answer: ['Alzheimers', 'Drug Abuse', 'Obesity', 'Type 2 Diabetes', 'Job Failures',],
+        correctAnswer: 'Alzheimers', 'Drug Abuse', 'Obesity', 'Type 2 Diabetes', 'Job Failures',
+        view: 'multiple-answer',
+        pointValue: 10,
+    },
+    {
+        question: 'ADHD is not the most common learning and behavior problem in children, and ' < b > 'ADULTS!'</b>,
+        answer: ['true', 'false',],
+        correctAnswer: 'false',
+        view: 'boolean',
+        pointValue: 10,
+    }
+
+]
