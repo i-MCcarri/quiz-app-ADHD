721b3768caabead81d68a2f43b5a99a62315ea00
Adyceum A. Magna Ccarri <61805757+i-MCcarri@users.noreply.github.com>
2020-08-13 07:56:35 -0700

    shoppinglist code test

--- index.js
+++ index.js
@@ -22,299 +22,114 @@
 
 // Create initial store
 
-console.clear();
-const DB = [
-	{
-		question: 'What is the closest star to our own sun?',
-		answers: [
-			'Proxima Centauri',
-			'Our sun',
-			'Betelgeuse',
-			'Sirius '],
-		correctAnswer: 'Proxima Centauri'
-	},
-	{
-		question: 'Which U.S. President made the first telephone call to the moon?',
-		answers: [
-			'Henry Truman',
-			'Richard Nixon',
-			'Franklin D. Roosevelt',
-			'Jimmy Carter'
-			],
-		correctAnswer: 'Richard Nixon'
-	},
-	{
-		question: 'Betelgeuse and Rigel are the two giant stars in which constellation?',
-		answers: [
-			'Andromeda',
-			'Aries',
-			'Virgo',
-			'Orion'
-			],
-		correctAnswer: 'Orion'
-	},
-	{
-		question: 'In our solar system, which planet has the shortest day?',
-		answers: [
-			'Earth',
-			'Venus',
-			'Jupiter',
-			'Mercury'
-			],
-		correctAnswer: 'Jupiter'
-	},
-	{
-		question: 'Hale-Bopp is classified as which type of small Solar System body?',
-		answers: [
-			'Comet',
-			'Asteroid',
-			'Meteoroid',
-			'Meteor'
-			],
-		correctAnswer: 'Comet'
-	},
-	{
-		question: 'What is the name for the disc-shaped region of icy bodies that extends from Neptune to about 55 astronomical units from the Sun?',
-		answers: [
-			'Orion\'s Belt',
-			'Copernicus\' Belt',
-			'Caeser\'s Belt',
-			'Kuiper Belt'
-			],
-		correctAnswer: 'Kuiper Belt'
-	},
-	{
-		question: 'In our solar system which two planets are known as ice giants?',
-		answers: [
-			'Venus & Mars',
-			'Neptune & Pluto',
-			'Uranus & Neptune',
-			'Jupiter & Uranus'
-			],
-		correctAnswer: 'Uranus & Neptune'
-	},
-	{
-		question: '"Mare Tranquillitatis" is the Latin name for what feature found on Earth\'s moon?',
-		answers: [
-			'Sea of Tranquility',
-			'Chilled Mahi',
-			'Tranquil Mars',
-			'Calm horse'
-			],
-		correctAnswer: 'Sea of Tranquility'
-	},
-	{
-		question: 'Who were there first two astronauts that landed on the moon in 1969?',
-		answers: [
-			'Buzz Lightyear & Lance "Woody" Armstrong',
-			'Neil Armstrong & Buzz Aldrin',
-			'Tom Aldrin & Neil Diamond',
-			'Buzz Adderrall & Neil Aldrin'
-			],
-		correctAnswer: 'Neil Armstrong & Buzz Aldrin'
-	},
-	{
-		question: 'The Great Red Spot is a gigantic storm located on which planet in our solar system?',
-		answers: [
-			'Earth',
-			'Mars',
-			'Jupiter',
-			'Venus'
-			],
-		correctAnswer: 'Jupiter'
-	}
-]
-let count = 0;
-let spanCount = 0;
-let score = 0;
+'use strict';
 
-// set up initial Intro
-function renderIntro() {
-  $(".js-intro").show();
-  $(".js-question").hide();
-  $(".js-feedback").hide();
-  $(".js-evaluation").hide();
+function generateItemElement(item) {
+  return `
+    <li data-item-id="${item.id}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
 }
 
-// bind start quiz button
-function renderQuestion() {
-  $(".js-intro-submit").on("click", event => {
+
+function generateShoppingItemsString(shoppingList) {
+  console.log("Generating shopping list element");
+
+  const items = shoppingList.map((item) => generateItemElement(item));
+  
+  return items.join("");
+}
+
+
+function renderShoppingList() {
+  // render the shopping list in the DOM
+  console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
+}
+
+
+function addItemToShoppingList(itemName) {
+  console.log(`Adding "${itemName}" to shopping list`);
+  STORE.push({id: cuid(), name: itemName, checked: false});
+}
+
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    renderQuestionForm();
+    console.log('`handleNewItemSubmit` ran');
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
   });
 }
 
-// updates DOM element with spanCount
-function increaseSpanCount() {
-  spanCount++;
-  $(".js-question-counter").text(spanCount);
+function toggleCheckedForListItem(itemId) {
+  console.log("Toggling checked property for item with id " + itemId);
+  const item = STORE.find(item => item.id === itemId);
+  item.checked = !item.checked;
 }
 
-// create a reusable renderQuestion function
-function renderQuestionForm() {
-  increaseSpanCount();
-  showOnlyQuestionDiv();
-  $(".js-question").html(`
-    <form id='form'>
-    <fieldset>
-    <legend><h2>${DB[count].question}</h2></legend>
-    <div class='css-answers'>
-		<input id='answer1' type="radio" name='answer' value='${DB[count].answers[0]}' required>
-		<label for='answer1'>${DB[count].answers[0]}</label>
-    </div>
-    <div class='css-answers'>
-    <input id='answer2' type="radio" name='answer' value='${DB[count].answers[1]}' required>
-		<label for='answer2'>${DB[count].answers[1]}</label>
-    </div>
-    <div class='css-answers'>
-    <input id='answer3' type="radio" name='answer' value='${DB[count].answers[2]}' required>
-		<label for='answer3'>${DB[count].answers[2]}</label>
-    </div>
-    <div class='css-answers'>
-    <input id='answer4' type="radio" name='answer' value='${DB[count].answers[3]}' required>
-		<label for='answer4'>${DB[count].answers[3]}</label>
-    </div>
-    </fieldset>
-    <div class="controls">
-    <button class='button js-question-submit'>Submit</button>
-    </div>
-    </form>`);
+
+function getItemIdFromElement(item) {
+  return $(item)
+    .closest('li')
+    .data('item-id');
 }
 
-// show only the question div
-function showOnlyQuestionDiv() {
-  $(".js-intro").hide();
-  $(".js-feedback").hide();
-  $(".js-question").show();
-}
-
-// Set event listener to submit the question; evaluate answer for correctness, call to render feedback
-function submitQuestion() {
-  $(".js-question").on("submit", event => {
-    event.preventDefault();
-    const value = fetchRadioValue(event);
-    const answerIsCorrect = checkAnswer(value);
-    const feedbackText = renderFeedbackText(answerIsCorrect);
-    renderFeedback(feedbackText);
+function handleItemCheckClicked() {
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    console.log('`handleItemCheckClicked` ran');
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(id);
+    renderShoppingList();
   });
 }
 
-// fetches the text value of the radio button
-function fetchRadioValue(event) {
-  let checkedRadioButton = $(event.currentTarget)
-    .find("input:checked")
-    .val();
-  return checkedRadioButton;
+// delete item function 
+function deleteListItem(itemId) {
+  console.log (`Deleting list item with id ${itemId} from STORE`);
+  const objItem = STORE.findIndex(item => item.id === itemId);
+  STORE.splice(objItem, objItem+1);
 }
 
-// radio value is equal to correct answer in database
-function checkAnswer(answer) {
-  return answer === `${DB[count].correctAnswer}`;
-}
-
-// if statement returns rendered correct or incorrect answer text
-function renderFeedbackText(correctAnswer) {
-  let responseMsg = "";
-  if (correctAnswer) {
-    responseMsg = `<h2>Correct</h2>
-                <p>On to the next.</p>
-                <div class="controls">
-                <button class='button js-feedback-submit'>Next</button>
-                </div>`;
-    increaseScoreCount();
-  } else {
-    responseMsg = `<h2>Ouch, this is not right</h2>
-                <p>The correct answer is: '${DB[count].correctAnswer}'</p>
-                <div class="controls">
-                <button class='button js-feedback-submit'>Next</button>
-                </div>`;
-  }
-  return responseMsg;
-}
-
-// increases score on correct answer
-function increaseScoreCount() {
-  score += 10;
-  updateDomScore(count);
-}
-
-// updates DOM element with score
-function updateDomScore(count) {
-  $(".js-score-counter").text(score);
-}
-
-// function inserts correct/incorrect text into HTML
-function renderFeedback(response) {
-  $(".js-question").hide();
-  $(".js-feedback")
-    .show()
-    .html(response);
-  increaseDbCount();
-}
-
-// increases DB question index for renderQuestion
-function increaseDbCount() {
-  count++;
-}
-
-// feedback div button calls eval function
-function submitFeedback() {
-  $(".js-feedback").on("click", ".js-feedback-submit", () => {
-    ifMaxQuestionIsReached();
+function handleDeleteItemClicked() {
+  // this function will be responsible for when users want to delete a shopping list
+  // item
+  // Listen for when a user clicks the 'delete' button on an item.
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
+  //Retrieve the item's id from the data attribute.
+  const itemId = getItemIdFromElement(event.currentTarget);
+  //Delete the item from STORE.
+  deleteListItem(itemId);
+  //Re-render the shopping list.
+  renderShoppingList();
+  console.log('`handleDeleteItemClicked` ran');
   });
 }
 
-// evaluates if end of questions are reached. if so, call eval. if not, call renderquestionform
-function ifMaxQuestionIsReached() {
-  if (count === 10) {
-    showEval();
-    renderScore();
-  } else {
-    renderQuestionForm();
-  }
+
+// this function will be our callback when the page loads. it's responsible for
+// initially rendering the shopping list, and activating our individual functions
+// that handle new item submission and user clicks on the "check" and "delete" buttons
+// for individual shopping list items.
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
 }
 
-// show eval div
-function showEval() {
-  $(".js-feedback").hide();
-  $(".js-evaluation").show();
-}
-
-// renders eval text & final score
-function renderScore() {
-  $(".js-evaluation").html(`
-  <h2>You did it!</h2>
-  <h3>Your final score is ${score}.</h3>
-  <h3>That is ${score/10} out of 10.</h3>
-  <div class="controls">
-  <button class='button js-button-reload'>Play again</button>
-  </div>
-  `);
-}
-
-//  binds reload functions to final eval div button
-function reload() {
-  $(".js-evaluation").on("click", ".js-button-reload", () => {
-    resetAll();
-    renderIntro();
-  });
-}
-
-// resets all counters
-function resetAll() {
-  count = 0;
-  spanCount = 0;
-  score = 0;
-  $(".js-question-counter").text(0);
-  $(".js-score-counter").text(0);
-}
-
-function startApp() {
-  renderIntro();
-  renderQuestion();
-  submitQuestion();
-  submitFeedback();
-  reload();
-}
-
-$(startApp);
+// when the page loads, call `handleShoppingList`
+$(handleShoppingList);
