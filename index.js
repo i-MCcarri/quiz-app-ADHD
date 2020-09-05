a42f4b3bd5dbb4878ba669a1f89c5440b65043d2
Adyceum A. Magna Ccarri <61805757+i-MCcarri@users.noreply.github.com>
2020-09-04 21:29:56 -0700

    inputted score functionality, Questions number, question render, store form input answer, display correct answer, check answer, and start quiz on button click

--- index.js
+++ index.js
@@ -1,320 +1,195 @@
-import $ from jQuery;
-import store from './store.js';
-
-// jQuery quiz-app
-const store.popQuiz = require("./store.js");
-
-
-//global variables
-const score = 0;
-const wrongScore = 0;
-const qNum = 0;
-const name = '';
-
-$('.validate')on('click', scoreEachQuizAnswer())
-if (selected === correctAnswer) {
-  let verified = null;
-  verified = true;
-  score += 10;
-  <script>
-$(message() {
-  $('#resultMessage').dialog({
-    autoOpen: false;
-  });
-  $('#verifyMessage').on('click',() {
-    $('#resultMessage').dialog('open');
-  });
-});
-</script>
-
-<div class='resultMessage' id='resultMessage'> 
-<title>Verified</title>
-<p>The correct answer was:</p>
-  <ul>
-    <li>{store.popQuiz.correctAnswer[idx]}</li>
-  </ul>
-  <span> ${score} out of 10 points!</span>
-</div>
-
-return verified, `</script>
-
-<div class='resultMessage' id='resultMessage'> 
-<title>Verified</title>
-<p>The correct answer was:</p>
-  <ul>
-    <li>{store.popQuiz.correctAnswer[idx]}</li>
-  </ul>
-  <span> ${score} out of 10 points!</span>
-</div>`;
-
-} else {
-  wrongScore -= 10;
-  verified = false;
- <script>
-  $(message() {
-    $('#resultMessage').dialog({
-      autoOpen: false;
-    });
-    $('#verifyMessage').on('click',() {
-      $('#resultMessage').dialog('open');
-    });
-  });
-</script>
-
-  return verified, `<div class='resultMessage' id='resultMessage'> 
-    <title>Try Again...</title>
-    <p>The correct answer was:</p>
-      <ul>
-        <li>${store.popQuiz.correctAnswer[idx]}</li>
-      </ul>
-    <span> 0 out of 10 points...</span>
-  </div>`;
-};
-
-indexAnswers(popQuiz){
-  for (let i=0; popQuiz.length; i++){
-    var idx = i;
-  }
-  return idx;
-}
-
-$('.slides')on('click', generateQView(popQuiz) {
-  store.popQuiz.map(i)
-} )
- 
-jQuery.map(popQuiz, generateQView() {
-  
-});
-
-function generateQView(){
-  let question = store.popQuiz[currentQuestion];
-if (store.store.popQuiz.view === 'multiple-choice') {
-  return `<div class='slides'>
-  <form id='quizApp'>
-      <fieldset>
-          <legend class="question">{questions}</legend>
-          <div>
-              <input 
-                  type='radio'
-                  name='multipoleChoice'
-                  id={store.popQuiz.answer}
-                  class='answer__option'
-                  check ={selection} === store.popQuiz[i]correctAnswer} />
-              <label htmlFor={store.popQuiz.answer} class='answer__label'>
-                      {store.popQuiz.answer[0]} 
-              </label><br>
-              <input 
-                  type='radio'
-                  name='multipoleChoice'
-                  id={store.popQuiz.answer}
-                  class='answer__option'
-                  check ={selection} === store.popQuiz[i]correctAnswer} />
-              <label htmlFor={store.popQuiz.answer} class='answer__label'>
-                  {store.popQuiz.answer[1]} 
-              </label><br>
-              <input 
-                  type='radio'
-                  name='multipoleChoice'
-                  id={store.popQuiz.answer}
-                  class='answer__option'
-                  check ={selection} === store.popQuiz[i]correctAnswer} />
-              <label htmlFor={store.popQuiz.answer} class='answer__label'>
-                  {store.popQuiz.answer[2]} 
-              </label><br>
-              <input 
-                  type='radio'
-                  name='multipoleChoice'
-                  id={store.popQuiz.answer}
-                  class='answer__option'
-                  check ={selection} === store.popQuiz[i]correctAnswer} />
-              <label htmlFor={store.popQuiz.answer} class='answer__label'>
-                  {store.popQuiz.answer[3]} 
-              </label><br>
-          </div>
-          <div class='buttons'>
-              <div class='verifyMessage' id='verifyMessage'>
-                  <button type='button' class=''>Check Answer</button>
-                  <button type="submit">Next Question</button>
-              </div>
-          </div>
-      </fieldset>
-  </form>
-</div>`
-
-} else if (store.popQuiz.view === 'multiple-answer'){
-
-  return `<div class='slides'>
-<form id='quizApp'>
-    <fieldset>
-        <legend class="question">{questions}</legend>
-        <div>
-            <input 
-                type='checkbox'
-                name='multipoleChoice'
-                id={store.popQuiz.answer}
-                class='answer__option'
-                check ={selection} === popQuiz[i]correctAnswer} />
-            <label htmlFor={store.popQuiz.answer} class='answer__label'>
-                    {store.popQuiz.answer[0]} 
-            </label><br>
-            <input 
-                type='checkbox'
-                name='multipoleChoice'
-                id={store.popQuiz.answer}
-                class='answer__option'
-                check ={selection} === popQuiz[i]correctAnswer} />
-            <label htmlFor={store.popQuiz.answer} class='answer__label'>
-                {store.popQuiz.answer[1]} 
-            </label><br>
-            <input 
-                type='checkbox'
-                name='multipoleChoice'
-                id={store.popQuiz.answer}
-                class='answer__option'
-                check ={selection} === popQuiz[i]correctAnswer} />
-            <label htmlFor={store.popQuiz.answer} class='answer__label'>
-                {store.popQuiz.answer[2]} 
-            </label><br>
-            <input 
-                type='checkbox'
-                name='multipoleChoice'
-                id={store.popQuiz.answer}
-                class='answer__option' />
-            <label htmlFor={store.popQuiz.answer} class='answer__label'>
-                {store.popQuiz.answer[3]} 
-            </label><br>
-        </div>
-        <div class='verifyMessage' id=''></div>
-    </fieldset>
-</form>` 
-} else if (store.store.popQuiz.view === 'boolean'){
-return `<div class="slides">
-<form id='quizApp'>
-<fieldset>
-    <legend class="question">{questions}</legend>
-    <div>
-        <input 
-            type='radio'
-            name='multipoleChoice'
-            id={answers}
-            class='answer__option'
-            check ={selection} === popQuiz[i]correctAnswer} />
-        <label htmlFor={answers} class='answer__label'>
-                {answers[0]} 
-        </label><br>
-        <input 
-            type='radio'
-            name='multipoleChoice'
-            id={answers}
-            class='answer__option'
-            check ={selection} === popQuiz[i]correctAnswer} />
-        <label htmlFor={answers} class='answer__label'>
-            {answers[1]} 
-        </label><br>
-    </form>
-  </div>`;
-} else {
-  //conclusion page
-  quizConclusion() {
-
-  }
-}
-}
+const i = STORE.currentQuestion;
+const q = STORE.currentQuestion;
+const displayedQ = q+1;
 
 function generateStartPage() {
-  return `<div class="startPage">
-  <h2>Who is ready to begin?</h2>
-  <p>Our subject is A.D.H.D.</p>
-  <form id="js-quiz-app-form-name">
-      <label for="name">Name:</label>
-      <input type="text" name="quizTakerName" class="inputBox" placeholder="First Name" required /><br><br>
-  </form>
-  <form id="js-quiz-app-form-id">
-      <label for="name">I.D.: </label>
-      <input type="text" name="idNum" class="inputBox" placeholder="Student ID"><br><br>
-  </form>
-  <form class="js-quiz-app-form-bottons" id="js-quiz-app-form-bottons">
-      <button class="exit-button" type="button">Exit</button>
-      <button class="start-quiz-button" type="submit">Start Quiz</button>
-  </form>
-  <form class="js-quiz-app-form-checkbox" id="js-quiz-app-form-checkbox">
-      <input type="checkbox" name="terms" required />
-      <label for="terms">sign your life away. . . again.</label>
-  </form>
-</div>`;
+  return `<div class='startPage'>
+            <fieldset>
+                <legend class="startQuestion">Pop Quiz!</legend>
+                <div></div>
+                <div class='buttons'>
+                    <div>
+                      <form id='start'>                    
+                        <button type="submit" class='startQuiz' id='startQuiz' value='submit'>Begin Quiz</button>
+                      </form>
+                    </div>
+                </div>
+            </fieldset>
+          </div>`;
 }
 
-function quizConclusion(){
-  return `<div class="quiz-conclusion">
-  <h2> Quiz Completed</h2>
-  <p>Congratulations ${name}!</p>
-  <p>Your final score was ${score} out of 100!</p>
-  <p>You answered ${wrongScore} questions wrong.</p>
-  <form class="js-quiz-app-form-bottons" id="js-quiz-app-form-bottons">
-      <button class="exit-button" type="button">Exit</button>
-      <button class="start-quiz-button" type="submit">Restart Quiz</button>
-  </form>
-</div>`;
+function startQuiz() {
+  $('main').on('click', '#startQuiz', event => {
+    event.preventDefault();
+    renderList();
+  });
 }
+
+function handleStoreAnswer(){
+  $('main').on('change', '.form', event => {
+    event.preventDefault;
+    const selected = $('input:checked');
+    const answer = selected.val();
+    submitAnswer(answer);
+  });
+}
+
+function handleCheckAnswer() {
+  $('main').on('click', '#checkAnswer', event => {
+    event.preventDefault();
+    $('.answerReveal').html(`
+      <div class='modal'>
+        <div class='modalWrapper'>
+          <legend class='modalHeader'><h5 class='modalAnswerIntro'></h5></legend>
+          <fieldset>
+            <p class='modalAnswerResult'></p>
+          </fieldset>
+        </div>
+      </div>`
+    );
+    $('.modalQuestionCount').html(`Question: ${displayedQ} / 10`);
+    $('.modalAnswerIntro').html('the correct answer is:');
+    $('.modalAnswerResult').html(`${STORE.questions[i].correctAnswer}`);
+    $('.modalScore').html(`Score: ${STORE.score} / 100`);
+      
+  });
+}
+
+function generateQView(){ 
+  
+  return `
+  <div class='modalQuestionCount'>Question: ${displayedQ} / 10</div>
+  <div class='box'></div>
+    <div class='slides'>
+      <form class='form' id='quizApp'>
+        <fieldset>
+            <legend class="question">${STORE.questions[i].question}</legend>
+            <div>
+                <input 
+                    type='radio'
+                    name='answers'
+                    id='answers'
+                    class='answer__option'
+                    value='${STORE.questions[i].answers[0]}' 
+                    />
+                <label htmlFor='answers' class='answer__label'>
+                        ${STORE.questions[i].answers[0]} 
+                </label><br>
+                <input 
+                    type='radio'
+                    name='answers'
+                    id='answers'
+                    class='answer__option'
+                    value='${STORE.questions[i].answers[1]}'
+                    />
+                <label htmlFor='answers' class='answer__label'>
+                ${STORE.questions[i].answers[1]} 
+                </label><br>
+                <input 
+                    type='radio'
+                    name='answers'
+                    id='answers'
+                    class='answer__option'
+                    value='${STORE.questions[i].answers[2]}'
+                    />
+                <label htmlFor='answers' class='answer__label'>
+                ${STORE.questions[i].answers[2]} 
+                </label><br>
+                <input 
+                    type='radio'
+                    name='answers'
+                    id='answers'
+                    class='answer__option'
+                    value='${STORE.questions[i].answers[3]}'
+                    required />
+                <label htmlFor='answers' class='answer__label'>
+                ${STORE.questions[i].answers[3]} 
+                </label><br>
+              </div>
+            </div>
+            <div class='answerReveal'></div>
+            <hr>
+            <div class='buttonWrapper'>
+              <div class='buttons' id='buttons'>
+                <button type='button' id='checkAnswer'>Check Answer</button>
+                <button type='submit' class='nextQ'>Next Question</button>
+              </div>
+            </div>
+            <div clas='scoreWrapper'>
+              <p class='modalScore'>Score: ${STORE.score} / 100</p>
+            </div>
+          </div>
+        </fieldset>
+    </form>
+  </div>`;
+}
+
+// function quizConclusion(){
+//   return `<div class="quizConclusion">
+//             <form id='end'>
+//               <fieldset>
+//                 <legend class='conclusion'>Quiz Completed</legend>
+//                 <div>
+//                   <p>Congratulations!</p>
+//                   <p>Your final score was ${STORE.questions.score} out of 100!</p>
+//                   <p>You answered ${STORE.questions.wrongScore} wrong.</p>
+//                 </div>
+//                 <div>
+//                   <button class="quizConlude" type="submit">Restart Quiz</button>
+//                 </div>
+//               </fieldset>
+//             </form>
+//           </div>`;
+// }
 
 function renderList(){
-  let html = generateQView();
-  console.log(html);
-  $('main').html(html);
+  $('main').html(generateQView());
 }
 
 function main() {
-  let startPage = generateStartPage();
-  $('main').html(startpage);
+  $('main').html(generateStartPage());
 }
 
-function SubmitAnswer(event) {
-  event.preventDefault();
-  let answer = $('input[name=store.popQuiz.answer]:checked').val();
-  if(store.store.popQuiz[store.currentQuestions].correctAnswer == answer){
-    alert(`You are correct!`);
+function submitAnswer(answer) {
+  console.log(answer);
+  if(STORE.questions[i].correctAnswer == answer){
+    console.log('correct');
+    //correct answer result
+    //update value of score
+    STORE.score += 10;
   } else {
-    alert(`oOf! The correct answer is: ${store.store.popQuiz[currentQuestion].correctAnswer}.`)
+    console.log('incorrect');
+    //incorrect answer result
+    //update value of wrongScore
+    STORE.wrongScore += 1;
+    $('.score').html(`Score: ${STORE.score}/100`);
   }
-  store.currentQuestion++;
-  renderList();
 }
 
-function completeItem(){
-  console.log($(this).parent());
-  alert('complete');
-  renderList();
-}
 
-//Requirements:
-//Button to start quiz
-  //when startQuiz button is clicked
-  //verify name was typed
-  //has check box been checked
-  //main()
-//user cannot skip questions without answering
-//answer 5+ questions of multiple choice
-//display current question number and ratio to total questions
-//see current test score
-//what happens if the answer is correct
-//what happens if the answer is wrong
-//received modal feedback, show correct answer
-//move to the next question on answer submit
-//end page / display total score
-//start new quiz
+// function startAgain() {
+//   $('main').on('click', '.quizConclude', () => {
+//     if (STORE.questions.currentQuestion === STORE.questions.questions.length) {
+//       //reset currentQuestion
+//       STORE.questions.currentQuestion = 1;
+//       //reset score
+//       STORE.questions.score = 0;
+//       //reset wrongScore
+//       STORE.questions.wrongScore = 0;
+//       let quizConclude = generateStartPage();
+//       $('main').html(quizConclude);
+//     }
+//     startQuiz();
+//   });
+// }
 
-//Technical Requirements:
-//Render answer choices in a <form>
-//use semantic HTML along with CSS and jQuery
-//be accessible as possible
-//associate `label` tags with inputs
-//use responsive design
-//be fully useable by keyboard tab through
 
-$('main').on('click','.start-quiz-button', function() {
-  renderList();
-});
 
-$('main').on('submit', '.form', submitAnswer);
+function eventHandle() { 
+  main();
+  startQuiz();
+  handleCheckAnswer();
+  handleStoreAnswer();
+};
 
-$(function() { main() });
+$(eventHandle);
